a174cadeb33672b21b8ed649b9d29d4e30df9436
Ajith Anand <ajithanand303@gmail.com>
2023-07-02 23:36:23 +0530

    regexp from in a range 7

--- sql/regexp.sql
+++ sql/regexp.sql
@@ -56,3 +56,11 @@ where city regexp '[^aeiouAEIOU]$';
 select distinct city 
 from station
 where city regexp '^[^aeiouAEIOU]|[^aeiouAEIOU]$';
+
+
+-- Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
+-- https://www.hackerrank.com/challenges/weather-observation-station-12/problem
+
+select distinct city 
+from station
+where city regexp '^[^aeiouAEIOU].*[^aeiouAEIOU]$';
