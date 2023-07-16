681f386717fe663fd979bfe7ddad339becdcd090
Ajith Anand <ajithanand303@gmail.com>
2023-07-16 23:12:30 +0530

    two sum unordered array

--- array/two_sum.cpp
+++ array/two_sum.cpp
@@ -3,17 +3,28 @@ using namespace std;
 #define endl "\n"
 
 vector<int> solve (vector<int> &nums, int target) {
-    vector<int>seen;
-    int pos=0;
-    for(int i : nums){
-        auto idx = std::find(seen.begin(), seen.end(),i);
-        if (idx != seen.end()){
-            return vector<int>{(int)(idx-seen.begin()),pos};
+    // vector<int>seen;
+    // int pos=0;
+    // for(int i : nums){
+    //     auto idx = std::find(seen.begin(), seen.end(),i);
+    //     if (idx != seen.end()){
+    //         return vector<int>{(int)(idx-seen.begin()),pos};
+    //     }
+    //     seen.push_back(target-i);
+    //     pos++;
+    // }
+    // return vector<int>{0};
+
+    unordered_map<int,int>seen;
+    for (int i= 0; i < nums.size(); ++i) {
+        if(seen.find(nums[i]) != seen.end()){
+            return vector<int>{seen[nums[i]],i};
         }
-        seen.push_back(target-i);
-        pos++;
+        seen[target-nums[i]]=i;
     }
     return vector<int>{0};
+
+    
 }
 
 int main(){
