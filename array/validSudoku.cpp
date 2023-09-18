93196820171441fa14dd087cb0a0db3010aad687
Ajith Anand <ajithanand303@gmail.com>
2023-09-18 17:28:09 +0530

    updated valid sudoku

--- array/validSudoku.cpp
+++ array/validSudoku.cpp
@@ -55,16 +55,37 @@ class Solution {
         return true;
     }
 public:
+    // bool isValidSudoku(vector<vector<char>>& board) {
+    //     for (int i =0;i<9;i++){
+    //         for(int j =0 ;j<9;j++){
+    //             if (board[i][j] == '.') continue;
+    //             if((!rowValid(board[i],board[i][j])) || (!colValid(board,j,board[i][j])) || (!miniBox(board,i,j,board[i][j])) ){
+    //                 return false;
+    //             }
+    //         }
+    //     }
+    //     return true;
+    // }
     bool isValidSudoku(vector<vector<char>>& board) {
-        for (int i =0;i<9;i++){
-            for(int j =0 ;j<9;j++){
-                if (board[i][j] == '.') continue;
-                if((!rowValid(board[i],board[i][j])) || (!colValid(board,j,board[i][j])) || (!miniBox(board,i,j,board[i][j])) ){
-                    return false;
-                }
+        unordered_map<int,unordered_set<char>>cols;
+        unordered_map<int,unordered_set<char>>rows;
+        map<pair<int,int>,unordered_set<char>>box;
+        for(int r =0;r<9;r++){
+            for(int c=0;c<9;c++){
+                if (board[r][c] == '.')continue;
+                if( (rows[r].find(board[r][c]) != rows[r].end())  ||
+                    (cols[c].find(board[r][c]) != cols[c].end())  ||
+                    (box[{r/3,c/3}].find(board[r][c]) != box[{r/3,c/3}].end())
+                   )
+                   return false;
+                
+                cols[c].insert(board[r][c]);
+                rows[r].insert(board[r][c]);
+                box[{r/3,c/3}].insert(board[r][c]);
             }
         }
         return true;
+        
     }
 };
 int main(){
