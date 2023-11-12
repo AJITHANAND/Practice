308e20353a5019aa742858eb67b44f64216258c3
Ajith Anand <ajithanand303@gmail.com>
2023-11-12 21:41:02 +0530

    updated with binary search

--- array/Insertion_position.cpp
+++ array/Insertion_position.cpp
@@ -5,13 +5,30 @@ using namespace std;
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int i =0;
-        for( ; i<nums.size();i++){
-            if(target <=nums[i]){
-                return i;
+        
+        int low = 0;
+        int high = nums.size()-1;
+        int mid;
+        while(low<=high){
+            mid = (low+high)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] < target){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
             }
         }
-        return i;
+        return low;
+        // int i =0;
+        // for( ; i<nums.size();i++){
+        //     if(target <=nums[i]){
+        //         return i;
+        //     }
+        // }
+        // return i;
     }
 };
 
@@ -19,7 +36,7 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     vector<int> nums = {1,3,5,6};
-    int target = 5;
+    int target = 2;
     Solution s;
     cout<<s.searchInsert(nums,target);
     return 0;
