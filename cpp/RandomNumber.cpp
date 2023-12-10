929d91e2117cc9d8849512b7ef83d619bda129ea
Ajith Anand <ajithanand303@gmail.com>
2023-12-10 10:21:13 +0530

    random number generator in cpp

---
+++ cpp/RandomNumber.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int Max_Random = 7;
+    srand(time(0));
+    cout<<"Random number between 0 - 6 :"<<rand()%Max_Random<<endl;
+  
+  
+  
+    return 0;
+}
