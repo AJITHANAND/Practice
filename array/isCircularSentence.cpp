271e950dfe7eab2824a0656094f72192a121648e
Ajith Anand <ajithanand303@gmail.com>
2024-11-03 00:00:39 +0530

    Update isCircularSentence.cpp
    more optimal using string_view

--- array/isCircularSentence.cpp
+++ array/isCircularSentence.cpp
@@ -2,22 +2,46 @@
 using namespace std;
 
 
+// Brute Force Solution
+// class Solution {
+// public:
+//     bool isCircularSentence(string str) {
+//         if (str.size() < 2) {
+//             return true;
+//         }
+//         for (int i = 1; i < str.size(); i++) {
+//             if (str[i]==' ') {
+//                 if (str[i-1] != str[(i+1)]){
+//                     return false;
+//                 }
+//             }else if (i == str.size()-1){
+//                 if (str[i] != str[0]) {
+//                     return false;
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+// };
+
+// Optimized Solution C++17 string_view
+
 class Solution {
 public:
-    bool isCircularSentence(string str) {
+    bool isCircularSentence(string_view str) {
         if (str.size() < 2) {
             return true;
         }
-        for (int i = 1; i < str.size(); i++) {
-            if (str[i]==' ') {
-                if (str[i-1] != str[(i+1)]){
-                    return false;
-                }
-            }else if (i == str.size()-1){
-                if (str[i] != str[0]) {
-                    return false;
-                }
+        if (str.back() != str.front()) {
+            return false;
+        }
+
+        size_t pos = str.find(' ');
+        while (pos != string::npos) {
+            if (str[pos-1] != str[pos+1]) {
+                return false;
             }
+            pos = str.find(' ', pos+1);
         }
         return true;
     }
