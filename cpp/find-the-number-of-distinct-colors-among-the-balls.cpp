582857987e2878f88e728aad9c4bae33cd145148
Ajith Anand <ajithanand303@gmail.com>
2025-02-07 23:32:12 +0530

    some optz

--- cpp/find-the-number-of-distinct-colors-among-the-balls.cpp
+++ cpp/find-the-number-of-distinct-colors-among-the-balls.cpp
@@ -6,31 +6,54 @@
 using namespace std;
 
 
+// class Solution {
+//     public:
+//         vector<int> queryResults(int limit, vector<vector<int>>& queries) {
+//             std::unordered_map<int, int> indexToColor;
+//             std::unordered_map<int, int> colorCount;
+//             vector<int> result;
+//             for(int i =0; i< queries.size(); i++){
+//                 int color = queries[i][1];
+//                 int index = queries[i][0];
+                
+//                 if(indexToColor.find(index) != indexToColor.end()){
+//                   int oldColor = indexToColor[index];
+//                   colorCount[oldColor]--;
+//                     if(colorCount[oldColor] == 0){
+//                         colorCount.erase(oldColor);
+//                     }
+//                 }
+//                 indexToColor[index] = color;
+//                 colorCount[color]++;
+//                 result.push_back(colorCount.size());
+//             }
+//             return result;
+//         }
+// };
+
+
+// optimized
 class Solution {
     public:
         vector<int> queryResults(int limit, vector<vector<int>>& queries) {
-            std::unordered_map<int, int> indexToColor;
-            std::unordered_map<int, int> colorCount;
-            vector<int> result;
-            for(int i =0; i< queries.size(); i++){
-                int color = queries[i][1];
-                int index = queries[i][0];
-                
-                if(indexToColor.find(index) != indexToColor.end()){
-                  int oldColor = indexToColor[index];
-                  colorCount[oldColor]--;
-                    if(colorCount[oldColor] == 0){
-                        colorCount.erase(oldColor);
-                    }
+            auto ball_to_color = unordered_map<int, int>{};
+            auto color_count = unordered_map<int, int>{};
+            auto result = vector<int>{};
+            int num_colors = 0;
+            result.reserve(queries.size());
+            for(const auto& query : queries){
+                auto [ball, color] = make_pair(query[0], query[1]);
+                auto &old_color = ball_to_color[ball];
+                if(old_color){
+                    if(--color_count[old_color] == 0) --num_colors;
                 }
-                indexToColor[index] = color;
-                colorCount[color]++;
-                result.push_back(colorCount.size());
+                ball_to_color[ball] = color;
+                if(++color_count[color] == 1) ++num_colors;
+                result.push_back(num_colors);
             }
             return result;
         }
 };
-
 
 int main(){
 
