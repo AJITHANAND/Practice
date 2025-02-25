7575546c7cf1cc3c22cd10f6002007d3c820b2da
Ajith Anand <ajithanand303@gmail.com>
2025-02-25 23:42:19 +0530

    dp solution

--- dp/number-of-sub-arrays-with-odd-sum.cpp
+++ dp/number-of-sub-arrays-with-odd-sum.cpp
@@ -1,24 +1,48 @@
 #include <iostream>
 using namespace std;
 
+// Bruteforce solution
+// class Solution {
+// public:
+//   int numOfSubarrays(vector<int> &arr) {
+//     int n = arr.size();
+//     int count = 0;
+//     for(int i = 0; i<n; i++) {
+//       for(int j = i; j<n; j++) {
+//         int sum = 0;
+//         for(int k = i; k <= j; k++) {
+//             sum += arr[k];
+//         }
+//         if (sum % 2 != 0) {
+//             count++;
+//         }
+//       }
+//     }
+//     return count;
+//   }
+// };
+
+// dp solution
 
 class Solution {
 public:
   int numOfSubarrays(vector<int> &arr) {
     int n = arr.size();
-    int count = 0;
+    long long count = 0;
+    int odd = 0;
+    int even = 1;
+    long long sum = 0;
     for(int i = 0; i<n; i++) {
-      for(int j = i; j<n; j++) {
-        int sum = 0;
-        for(int k = i; k <= j; k++) {
-            sum += arr[k];
-        }
-        if (sum % 2 != 0) {
-            count++;
-        }
+      sum += arr[i];
+      if (sum % 2 == 0) {
+        count += odd;
+        even++;
+      } else {
+        count += even;
+        odd++;
       }
     }
-    return count;
+    return count % 1000000007;
   }
 };
 
