35b35702c6351168b660104766151e16b6cb9dfa
Ajith Anand <ajithanand303@gmail.com>
2025-03-08 23:32:56 +0530

    removed queue ds

--- sliding window/minimum-recolors-to-get-k-consecutive-black-blocks.cpp
+++ sliding window/minimum-recolors-to-get-k-consecutive-black-blocks.cpp
@@ -3,29 +3,56 @@
 #include <vector>
 using namespace std;
 
+// class Solution {
+// public:
+//     int minimumRecolors(string blocks, int k) {
+//         std::queue<char>window;
+// 	int minimum = INT_MAX;
+// 	int currentWhiteCount = 0;
+// 	for(int i =0;i<blocks.size();i++){
+// 		if (window.size() == k ){
+// 			minimum = std::min(minimum,currentWhiteCount);
+// 			if(window.front() == 'W'){
+// 				currentWhiteCount--;
+// 			}
+// 			window.pop();
+// 		}
+// 		if(blocks[i] == 'W'){
+// 			currentWhiteCount++;
+// 		}
+// 		window.push(blocks[i]);
+// 	}
+// 	minimum = std::min(minimum,currentWhiteCount);
+// 	return minimum;
+//     }
+// };
+
 class Solution {
-public:
-    int minimumRecolors(string blocks, int k) {
-        std::queue<char>window;
-	int minimum = INT_MAX;
-	int currentWhiteCount = 0;
-	for(int i =0;i<blocks.size();i++){
-		if (window.size() == k ){
-			minimum = std::min(minimum,currentWhiteCount);
-			if(window.front() == 'W'){
-				currentWhiteCount--;
+	public:
+		int minimumRecolors(string blocks, int k) {
+			int n = blocks.size();
+			int whiteCount = 0;
+			for (int i = 0; i < k && i < n; i++) {
+				if (blocks[i] == 'W') {
+					whiteCount++;
+				}
 			}
-			window.pop();
+			int minWhiteCount = whiteCount;
+			for (int i = k; i < n; i++) {
+				if (blocks[i - k] == 'W') {
+					whiteCount--;
+				}
+				
+				if (blocks[i] == 'W') {
+					whiteCount++;
+				}
+				minWhiteCount = min(minWhiteCount, whiteCount);
+			}
+			
+			return minWhiteCount;
 		}
-		if(blocks[i] == 'W'){
-			currentWhiteCount++;
-		}
-		window.push(blocks[i]);
-	}
-	minimum = std::min(minimum,currentWhiteCount);
-	return minimum;
-    }
-};
+	};
+	
 
 int main(){
 
